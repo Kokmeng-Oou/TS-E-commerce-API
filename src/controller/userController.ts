43691cac6f7b91de9bd7c6ed1339697ed602136d
Kokmeng.Oou <kokmeng.ou69@gmail.com>
2023-11-04 07:00:21 +0000

    fix updateUser

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -25,24 +25,9 @@ export const ShowCurrentUser = asyncWrapper(
   }
 )
 
-export const updateUser = asyncWrapper(async (req: Request, res: Response) => {
-  const { prevPassword, currentPassword } = req.body
-  if (!prevPassword || !currentPassword)
-    throw new BadRequestError('Please provide both.value')
-
-  const user = await User.findOne({ _id: (req as any).user.userId })
-  if (!user)
-    throw new UnauthenticatedError(
-      'You are not logged in or your session has expired!'
-    )
-  const isPrevPasswordMatch: boolean = await user.comparePassword(prevPassword)
-  if (!isPrevPasswordMatch)
-    throw new UnauthenticatedError('Invalid Credentials')
-
-  user.password = currentPassword
-  await user.save()
-  res.status(StatusCodes.OK).json({ msg: 'Password successfully updated' })
-})
+export const updateUser = asyncWrapper(
+  async (req: Request, res: Response) => {}
+)
 
 export const updateUserPassword = asyncWrapper(
   async (req: Request, res: Response) => {
