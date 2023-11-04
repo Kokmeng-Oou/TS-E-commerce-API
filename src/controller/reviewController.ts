ccb871c4bc3e732ae0bd6b8d4f2d9fd6e8aa4c23
Kokmeng.Oou <kokmeng.ou69@gmail.com>
2023-11-04 11:25:20 +0000

    create review

--- src/controller/reviewController.ts
+++ src/controller/reviewController.ts
@@ -1,9 +1,28 @@
 import asyncWrapper from '../middleware/async'
 import { Request, Response } from 'express'
+import Review from '../models/Review'
+import Product from '../models/Product'
+import { StatusCodes } from 'http-status-codes'
+import { BadRequestError, NotFoundError } from '../errors'
+import { checkPermissions } from '../utils'
 
 const createReview = asyncWrapper(
   async (req: Request, res: Response): Promise<void> => {
-    res.send('createReview')
+    const { product: productId } = req.body
+    const isValidProduct = await Product.findOne({ _id: productId })
+    if (!isValidProduct)
+      throw new NotFoundError(`No products with id : ${productId}`)
+
+    const alreadySubmitted = await Review.findOne({
+      product: productId,
+      user: (req as any).user._id,
+    })
+    if (alreadySubmitted)
+      throw new NotFoundError(`Already Submitted review for this product`)
+
+    req.body.user = (req as any).user.userId
+    const review = await Review.create(req.body)
+    res.status(StatusCodes.CREATED).json({ review })
   }
 )
 
